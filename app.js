0595e0b6424572885f1884e96ceb3e5e0e60b447
Shane Chin <sac2171@gmail.com>
2013-07-20 21:15:56 +0000

    added facebook keys and adding mongo

--- app.js
+++ app.js
@@ -7,9 +7,39 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , passport = require('passport')
+  , mongoose = require('mongoose')
+  , FacebookStrategy = require('passport-facebook').Strategy
   , path = require('path');
 
+var secret = require('./PRIVATE');
+console.log(secret);
 var app = express();
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  var userSchema = mongoose.Schema({
+        name: String
+  });
+  var User = mongoose.model('User', userSchema);
+});
+
+
+passport.use(new FacebookStrategy({
+      clientID: secret.facebook.id,
+      clientSecret: secret.facebook.secret, 
+      callbackURL: "http://padapt.com/auth/facebook/callback"
+    },
+      function(accessToken, refreshToken, profile, done) {
+            User.findOrCreate(function(err, user) {
+                    if (err) { return done(err); }
+                          done(null, user);
+                              });
+              }
+));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -29,6 +59,11 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', 
+      passport.authenticate('facebook', { successRedirect: '/',
+                                              failureRedirect: '/login' }));
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
